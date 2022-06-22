9ffbbf32a9a4a3841292906005d4fd26cafd5659
austinried <4966622+austinried@users.noreply.github.com>
2022-06-22 12:48:31 +0900

    don't skip on playback error

--- just_audio/android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
+++ just_audio/android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
@@ -382,18 +382,18 @@ public class AudioPlayer implements MethodCallHandler, Player.Listener, Metadata
             Log.e(TAG, "default PlaybackException: " + error.getMessage());
             sendError(String.valueOf(error.errorCode), error.getMessage());
         }
-        errorCount++;
-        if (player.hasNextMediaItem() && currentIndex != null && errorCount <= 5) {
-            int nextIndex = currentIndex + 1;
-            Timeline timeline = player.getCurrentTimeline();
-            // This condition is due to: https://github.com/ryanheise/just_audio/pull/310
-            if (nextIndex < timeline.getWindowCount()) {
-                // TODO: pass in initial position here.
-                player.setMediaSource(mediaSource);
-                player.prepare();
-                player.seekTo(nextIndex, 0);
-            }
-        }
+//        errorCount++;
+//        if (player.hasNextMediaItem() && currentIndex != null && errorCount <= 5) {
+//            int nextIndex = currentIndex + 1;
+//            Timeline timeline = player.getCurrentTimeline();
+//            // This condition is due to: https://github.com/ryanheise/just_audio/pull/310
+//            if (nextIndex < timeline.getWindowCount()) {
+//                // TODO: pass in initial position here.
+//                player.setMediaSource(mediaSource);
+//                player.prepare();
+//                player.seekTo(nextIndex, 0);
+//            }
+//        }
     }
 
     private void completeSeek() {
